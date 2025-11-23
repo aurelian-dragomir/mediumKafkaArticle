91b966d8ef9a9507049e31ec2853b5c5286863f4
Aurelian Dragomir <dragomir.aurelian@gmail.com>
2025-11-23 19:05:34 +0200

    initial commit

---
+++ src/main/java/com/learn/kafka/transactions/LearnKafkaTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.learn.kafka.transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnKafkaTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnKafkaTransactionsApplication.class, args);
+	}
+
+}
