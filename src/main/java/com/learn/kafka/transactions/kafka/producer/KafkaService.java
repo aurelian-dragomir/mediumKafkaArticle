d1b5fbe6543530e4e886aa8fc465bfcf6cb14791
Aurelian Dragomir <dragomir.aurelian@gmail.com>
2025-11-24 13:21:11 +0200

    producer transactions

--- src/main/java/com/learn/kafka/transactions/kafka/producer/KafkaService.java
+++ src/main/java/com/learn/kafka/transactions/kafka/producer/KafkaService.java
@@ -2,19 +2,16 @@ package com.learn.kafka.transactions.kafka.producer;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Service
 @RequiredArgsConstructor
-@Component
 public class KafkaService {
-
     private final KafkaTemplate<Void, String> kafkaTemplate;
 
-    @Transactional
-    public void send(String... values) {
-        for (String value : values) {
-            kafkaTemplate.sendDefault(value);
-        }
+    @Transactional(transactionManager = "kafkaTransactionManager")
+    public void sendToKafka(String value) {
+        kafkaTemplate.sendDefault(value);
     }
 }
